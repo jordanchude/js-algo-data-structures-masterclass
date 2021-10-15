7c42d4673f542da881bda9f38a6bfc1af1e6da06
Jordan <jordan@chude.co>
2021-10-15 19:47:22 -0400

    binary search pseudocode

--- searching-algos.js
+++ searching-algos.js
@@ -10,17 +10,37 @@
 // ENDFUNCTION
 
 // Example
-function linearSearch(array, value){
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === value) {
-            return i
-        }
-    }
-    return -1;
-  }
+// function linearSearch(array, value){
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === value) {
+//             return i
+//         }
+//     }
+//     return -1;
+//   }
 
 // linear search test cases
-console.log(linearSearch([10, 15, 20, 25, 30], 15));
-console.log(linearSearch([1, 2, 3, 4, 5], 6));
+// console.log(linearSearch([10, 15, 20, 25, 30], 15));
+// console.log(linearSearch([1, 2, 3, 4, 5], 6));
 
 // Big O notation: O(n)
+// Linear search Big O: O(1) (best), O(n) (average), O(n) (worst)
+
+// BINARY SEARCH
+//Psuedocode
+// FUNCTION accepts sorted array and value 
+//     VARIABLE left equals 0
+//     VARIABLE right equals end of array
+
+//     WHILE left is bigger than right
+//         VARIABLE middle equals middle index
+//             IF value is middle
+//                 RETURN middle
+//             ELSEIF value is smaller than middle
+//                 right equals middle
+//             ELSEIF value is larger than middle 
+//                 left equals middle
+//             ENDIF
+//     ENDWHILE
+//     RETURN -1
+// ENDFUNCTION

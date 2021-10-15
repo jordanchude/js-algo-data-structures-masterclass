a1b9281e6d69300891ae563bef22e7a857d74364
Jordan <jordan@chude.co>
2021-10-15 19:15:01 -0400

    linear search example complete

--- searching-algos.js
+++ searching-algos.js
@@ -11,8 +11,15 @@
 
 // Example
 
-function linearSearch(){
-    // add whatever parameters you deem necessary - good luck!
+function linearSearch(array, value){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            return i
+        }
+    }
+    return -1;
   }
 
 // test cases
+console.log(linearSearch([10, 15, 20, 25, 30], 15));
+console.log(linearSearch([1, 2, 3, 4, 5], 6));

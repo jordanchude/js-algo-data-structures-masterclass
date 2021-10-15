70504b0b11bb77c83ce2b5a9f20798d4c4370d9a
Jordan <jordan@chude.co>
2021-10-15 19:11:44 -0400

    linear search pseudocode

---
+++ searching-algos.js
@@ -0,0 +1,18 @@
+// LINEAR SEARCH
+// Psuedocode
+// FUNCTION accepts array and value 
+//     LOOP through array
+//         IF current array element is equal to value
+//             return array element index
+//         ENDIF
+//     ENDLOOP
+//     RETURN -1
+// ENDFUNCTION
+
+// Example
+
+function linearSearch(){
+    // add whatever parameters you deem necessary - good luck!
+  }
+
+// test cases

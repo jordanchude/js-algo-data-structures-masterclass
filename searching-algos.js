31d80053e5d0ae99307ad143540a87611d1c75e5
Jordan <jordan@chude.co>
2021-10-19 20:10:48 -0400

    binary search example complete

--- searching-algos.js
+++ searching-algos.js
@@ -47,24 +47,24 @@
 
 // Example
 function binarySearch(arr, val){
-    var left = 0;
-    var right = arr.length - 1;
+    var start = 0;
+    var end = arr.length - 1;
 
-    while (left < right) {
-        var middle = Math.floor(left + (right - left)/2)
-        
+    while (start < end) {
+        var middle = Math.floor((start + end) / 2)
         if (arr[middle] === val) {
             return middle;
-        } else if (arr[middle] > val) {
-            right = middle - 1;
+        } else if (val < arr[middle]) {
+            end = middle - 1;
         } else {
-            left = middle + 1;
+            start = middle + 1;
         }
     }
     return -1;
-}
+};
 
 // test cases
-console.log(binarySearch([1, 2, 3, 4, 5], 2))
-console.log(binarySearch([1, 2, 3, 4, 5], 3))
+// console.log(binarySearch([1, 2, 3, 4, 5], 2))
+// console.log(binarySearch([1, 2, 3, 4, 5], 3))
 console.log(binarySearch([1, 2, 3, 4, 5], 6))
+// console.log(binarySearch([1, 2, 3, 4, 5], 1))
